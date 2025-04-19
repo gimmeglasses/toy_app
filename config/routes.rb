6d07623ff01bbb5f7bae78f2c48f73a5a1a8905e
gimmeglasses <50077615+gimmeglasses@users.noreply.github.com>
2025-04-19 10:05:20 +0000

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  # 第１０章 resources
+  # access ../users     -> users#indexs
+  # access ../users/new -> users#new
+  # access ../users/:id -> users#show
+  # ...
   root "hello#index"
 end
